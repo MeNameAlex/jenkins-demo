ba22ccac5a05028fbcd3c82c8ae1e843ce8d13d3
Alexander <meerpohl@saenger.io>
2021-07-23 07:10:54 +0200

    Test hinzugefügt

---
+++ jenkins-demo/src/test/java/com/example/jenkinsdemo/JenkinsDemoTest.java
@@ -0,0 +1,20 @@
+package com.example.jenkinsdemo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class JenkinsDemoTest {
+
+	String funktioniert = "JUnit funktioniert";
+	String funktioniertNicht = "JUnit funktioniert nicht";
+
+	@Test
+	public void testMethodeZumTestenMitErfolg(){
+		assertEquals("JUnit funktioniert", funktioniert);
+	}
+
+	@Test
+	public void testMethodeZumTestOhneErfolg(){
+		assertEquals("JUnit funktioniert", funktioniertNicht);
+	}
+}
